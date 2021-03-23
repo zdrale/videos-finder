dc4cb2bdc3efa689cd3f7644f35297f14ecbf273
zdrale <nikolazd22@outlook.com>
2021-03-23 12:05:33 +0100

    Added on term submit function
    --Function gets 5 videos from api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,8 +9,9 @@ class SearchBar extends React.Component {
     onFormSubmit = event => {
         event.preventDefault();
 
-        //TODO: Make sure to call callback from parent component
+        this.props.onFormSubmit(this.state.term);
         
+
     };
 
     render() {
